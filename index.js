7c86533fcf406bb5ab4d4654acac1c5d636731d4
+ <+>
2023-08-01 10:36:05 -0600

    finished work

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = () => {
+ const driversArray = ['Antonia', 'Nuru', 'Amari','Mo'];   
+ const theFirstTwo = driversArray.slice(0,2);
+  return theFirstTwo;
+} 
+
+const returnLastTwoDrivers = () => {
+    const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    const theLastTwo = driversArray.slice(-2);
+    return theLastTwo;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+        return fare * multiplier;
+    };
+}; 
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (driversArray, selectorFunction) => {
+    return selectorFunction(driversArray);
+}
